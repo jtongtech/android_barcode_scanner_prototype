85fd78899d923ec1c95a8a953b639e901e4e2491
JTong <eldertong@gmail.com>
2018-01-17 14:45:17 -0500

    non-working code for the image

---
+++ app/src/main/java/tongtechnologies/com/barcode/captureImageForConversion.java
@@ -0,0 +1,13 @@
+package tongtechnologies.com.barcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class captureImageForConversion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture_image_for_conversion);
+    }
+}
